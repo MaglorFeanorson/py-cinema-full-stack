e9e51be1f5c3f51c1d4a9133c029cdcdf1bed878
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-20 22:02:18 +0300

    solution

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -64,9 +64,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://127.0.0.1:5173",
-    # "http://localhost:5174",
-    "*"
+     "http://127.0.0.1:5173",
+     "http://localhost:5177",
+
 
 ]
 ROOT_URLCONF = "cinema_service.urls"
